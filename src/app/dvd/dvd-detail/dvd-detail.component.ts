5b64dcf65fac84d1dc6c4f5420095b4e2e2f8ee2
Örivelton Cesar <oconceicao@recordtv.com.br>
2019-08-01 17:16:54 -0300

    chore: Component loading, go back and button

--- src/app/dvd/dvd-detail/dvd-detail.component.ts
+++ src/app/dvd/dvd-detail/dvd-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { DvdService } from '../../services/dvd.service';
 import { Observable } from 'rxjs/internal/Observable';
 import { Dvd } from 'src/app/models/dvd';
@@ -13,12 +13,16 @@ export class DvdDetailComponent implements OnInit {
 
   dvd$: Observable<Dvd>;
 
-  constructor(private route: ActivatedRoute, private dvdService: DvdService) { }
+  constructor(private route: ActivatedRoute, private dvdService: DvdService, private router: Router) { }
 
   ngOnInit() {
     const index: number = +this.route.snapshot.paramMap.get('index');
     this.dvd$ = this.dvdService.get(index);
     // console.log('Index: ', this.route.snapshot.paramMap.get('index'));
     // this.route.paramMap.subscribe((params: ParamMap) => console.log('Index: ', params.get('index')));
+  }
+
+  goBack() {
+    this.router.navigate(['/dvds']);
   }
 }
