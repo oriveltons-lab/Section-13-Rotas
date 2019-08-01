bff3197635fb264a99ad01649dd821136702ce69
Örivelton Cesar <oconceicao@recordtv.com.br>
2019-08-01 17:56:01 -0300

    chore: Get param by Router, includ new information about fetching

--- src/app/dvd/dvd.component.ts
+++ src/app/dvd/dvd.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DvdService } from '../services/dvd.service';
 import { Observable } from 'rxjs/internal/Observable';
 import { Dvd } from '../models/dvd';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dvd',
@@ -12,10 +13,14 @@ export class DvdComponent implements OnInit {
 
   dvds$: Observable<Dvd[]>;
 
-  constructor(private dvdService: DvdService) { }
+  constructor(private dvdService: DvdService, private router: Router) { }
 
   ngOnInit() {
     this.dvds$ = this.dvdService.dvds$;
+  }
+
+  goDetails(i: number, dvd: Dvd) {
+    this.router.navigate([`dvds/${i}`, { title: dvd.title }]);
   }
 
 }
